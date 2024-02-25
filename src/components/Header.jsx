1e45e80a7eec6c39c3a020c52ff056f4973c5d00
retr013 <stagnirovat@gmail.com>
2024-02-25 15:59:49 +0000

    third commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+function Header() {
+    return (
+        <header className='header'>
+            <img className='logo' src="https://upload.wikimedia.org/wikipedia/sco/thumb/d/d3/Starbucks_Corporation_Logo_2011.svg/297px-Starbucks_Corporation_Logo_2011.svg.png?20170312192423" alt=""/>
+        </header>
+    )
+}
+
+export default Header;
---
+++ src/components/Profile.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div className='picB'>
+                <img className='picB-img' src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Aerial_view_of_Barcelona%2C_Spain_%2851227309370%29_%28cropped%29.jpg/1200px-Aerial_view_of_Barcelona%2C_Spain_%2851227309370%29_%28cropped%29.jpg" alt=""/>
+            </div>
+            <div className=''>
+                ava + desc
+            </div>
+            <div>
+                my posts
+                <div>new post</div>
+                <div>post1</div>
+                <div>post2</div>
+            </div>
+        </div>
+    );
+};
+
+export default Profile;
